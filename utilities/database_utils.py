7fa091e008d1101e5663116bdc979e9e35a6c82b
martynov <vvayfarer@yandex.com>
2017-12-14 13:24:35 -0500

    initial commit

---
+++ utilities/database_utils.py
@@ -0,0 +1,9 @@
+
+def create_database(database_path: str):
+    #TODO: generate the datbase
+    pass
+
+def save_words_to_database(database_path: str, word_list: list):
+    #TODO: save words to the database
+    pass
+
---
+++ utilities/url_utilities.py
@@ -0,0 +1,12 @@
+def load_urls_from_file(file_path: str):
+    #TODO: add the coded needed to read the text file with urls in it
+    pass
+
+
+    def load_page(url: str):
+        #TODO: add code that read url
+        pass
+
+def scrape_page(page_contents: str):
+    #TODO: analyze the text
+    pass
